11c3e11b22df2fd640e8b00877c24b758f9d2c26
Dwipaaa <147568071+Dwipaaa@users.noreply.github.com>
2023-11-13 11:20:19 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+        $username="root";
+        $password="";
+        $db="crud_daging";
+
+        define("DB_HOST", "localhost");
+        define("DB_USER", $username);
+        define("DB_PASS", $password);
+        define("DB_NAME", $db);
+        
+        
+
+?>
