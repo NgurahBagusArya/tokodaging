ac20eaa3382042a2296e7560add932bd3841e54c
Dwipaaa <147568071+Dwipaaa@users.noreply.github.com>
2023-11-13 11:20:14 +0800

    Create aboutdaging.php

---
+++ aboutdaging.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styledaging.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-light">
+        <div class="container-fluid">
+            <h5 class="ms-3">Meat Guy</h5>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <select class="form-select" aria-label="Default select example">
+                </select>
+            </button>
+
+            <!-- navbar -->
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active text-light" aria-current="page" href="/web-daging/daging.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="/web-daging/aboutdaging.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="#">Sign In</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- about -->
+    
+    <br><br><br><br><br>
+    <div class="section">
+        <div class="container mt-5">
+            <div class="d-flex flex-row-reverse align-items-center">
+                <div class="title">
+                    <h1>About</h1>
+                </div>
+                <div class="content ms-5">
+                    <div class="text">
+                        <h5>Kami dengan bangga mempersembahkan Meat Guy, destinasi terkemuka Anda untuk semua kebutuhan daging berkualitas tinggi. Sebagai pecinta daging yang berkomitmen, kami berdedikasi untuk menyediakan produk daging terbaik dan menghadirkannya langsung ke meja makan Anda. <br><br>
+                            Dengan pengalaman bertahun-tahun dalam industri daging, kami memahami pentingnya kualitas dan keselamatan makanan. Itulah mengapa kami bekerja sama dengan peternak lokal dan pemasok yang berbagi nilai kami dalam menghasilkan daging segar dan berkualitas. Semua produk kami diproses dengan standar kebersihan yang ketat dan melalui pengawasan ketat untuk memastikan bahwa Anda hanya mendapatkan yang terbaik.</h3>
+                    </div>
+                </div>
+                <div class="img">
+                    <img src="img/meat.jpg" class="rounded-4" height="400px">
+                </div>
+            </div>
+        </div>
+    </div>
+    <br><br><br><br><br><br><br><br><br>
+    <footer>
+        <div class="container text-center">
+            <div class="footer row px-4 justify-content-center">
+                <div class="col-4">
+                    <br><br><br>
+                    <ul>
+                        <li>Meat Guy</li>
+                        <br>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="/web-daging/daging.php">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="/web-daging/aboutdaging.php">ABOUT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">CONTACT US</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">SIGN IN</a>
+                        </li>
+                        <br><br><br><br><br>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <br><br><br>
+                    <ul>
+                        <li>CONTACT US</li>
+                        <br>
+                        <li>Email:meatguy22@gmail.com</li>
+                        <li>0876263232542</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <br><br><br>
+                    <ul>
+                        <li>Social Media</li>
+                        <br>
+                        <li>Instagram</li>
+                        <li>Facebook</li>
+                    </ul>
+
+
+                </div>
+            </div>
+        </div>
+        </div>
+        <hr>
+        <div class="copyrightText d-flex justify-content-between px-1 py-1">
+            <a>Copyright © 2022 - 2023 Gadget Store</a>
+            <a>Indonesia / Indonesia</a>
+        </div>
+    </footer>
+</body>
+
+</html>
