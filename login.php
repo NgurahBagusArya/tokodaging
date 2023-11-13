b023efa5bf0b6eeae4bab2d620f43be32d92ea35
CancriPiscium <nyenkadendwika@gmail.com>
2023-11-13 12:03:38 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,26 +1,26 @@
-<?php 
+<?php
 
-    require 'function.php';
+require 'function.php';
 
-    if (isset($_POST['login'])){
+if (isset($_POST['login'])) {
 
-        $email = $_POST["email"];
-        $password = $_POST["password"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-       $result = mysqli_query($conn, "SELECT * FROM pengguna WHERE email = '$email'");
+    $result = mysqli_query($conn, "SELECT * FROM pengguna WHERE email = '$email'");
 
-       // cek user name
-       if(mysqli_num_rows($result) === 1){
-           
-           $row = mysqli_fetch_assoc($result);
-           // cek password
-        if ( password_verify($password, $row["password"])) {
-            header ("location: daging.php");
+    // cek user name
+    if (mysqli_num_rows($result) === 1) {
+
+        $row = mysqli_fetch_assoc($result);
+        // cek password
+        if (password_verify($password, $row["password"])) {
+            header("location: daging.php");
             exit;
         }
-       }
-       $error = true;
     }
+    $error = true;
+}
 
 ?>
 
@@ -47,32 +47,22 @@
 </head>
 
 <body class="bg-gradient-primary">
-
     <div class="container">
-
         <div class="card o-hidden border-0 shadow-lg my-5">
-
-
-
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row justify-content-center">
-
-                    <div class="col-lg-5">
+                    <div class="col-lg-5 align-self-center">
                         <img class="w-100" src="img/gambar.png" alt="" styles="">
                     </div>
-
                     <div class="col-lg-7">
                         <div class="p-5">
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Login</h1>
-
-                                <?php if( isset ($error)) : ?>
+                                <?php if (isset($error)) : ?>
                                     <p styles="color: red; font-style: italic;"></p>user name / password salah</p>
                                 <?php endif; ?>
-                            
                                 <form action="" method="post">
-
                                     <div class="form-group">
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user" id="exampleInputEmail" name="email" placeholder="Email">
@@ -81,26 +71,18 @@
                                     <div class="form-group">
                                         <input type="password" class="form-control form-control-user" id="exampleInputEmail" name="password" placeholder="Password">
                                     </div>
-
                                     <button type="submit" class="btn btn-primary" name="login">Submit</button>
-
                                     <!-- <a href="login.php" class="btn btn-primary btn-user btn-block" name="registrasi">
                                         registrasi
                                     </a> -->
                                     <hr>
-                                    <p>
-                                       Don't have an account? <a href="register.php">registrasi</a>
-                                    </p>
-
+                                    <p>Don't have an account? <a href="register.php">registrasi</a></p>
                                 </form>
-
-
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
 
         <!-- Bootstrap core JavaScript-->
@@ -112,7 +94,7 @@
 
         <!-- Custom scripts for all pages-->
         <script src="js/sb-admin-2.min.js"></script>
-
+    </div>
 </body>
 
 </html>
