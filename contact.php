f1a940200ca7054011a81748e2fb37601fad027f
CancriPiscium <nyenkadendwika@gmail.com>
2023-11-13 11:07:13 +0800

    a

---
+++ contact.php
@@ -0,0 +1,61 @@
+<?php
+include('layouts/header.php');
+?>
+
+<body>
+    <div class="container text-center py-5">
+        <h3>Our Team</h3>
+        <h4 class="text-muted">Ini adalah Perkenalan singkat dari kami.</h4>
+        <div class="row row-cols-1 row-cols-md-3 g-4 py-5">
+            <div class="col">
+                <div class="card">
+                    <img src="img/PP2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">I Kadek Dwika Wira Ardana</h5>
+                        <p class="card-text">Tidak ada hal khusus yang saya miliki, yang bisa saya banggakan adalah saya tinggi</p>
+                    </div>
+                    <div class="d-flex justify-content-evenly p-4">
+                        <i class="bi bi-facebook"></i>
+                        <i class="bi bi-linkedin"></i>
+                        <i class="bi bi-envelope-fill"></i>
+                        <i class="bi bi-whatsapp"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img/PP2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Kadek Dwipa Wardana</h5>
+                        <p class="card-text">Berkumis, Suka Menjahili Teman-temannya, dan beliau adalah admin sungut lele</p>
+                    </div>
+                    <div class="d-flex justify-content-evenly p-4">
+                        <a href="#"><i class="bi bi-facebook" aria-hidden="true"></i></a>
+                        <a href="https://www.instagram.com/dwikaawiraardana_/"><i class="bi bi-instagram"></i></a>
+                        <i class="bi bi-envelope-fill"></i>
+                        <i class="bi bi-whatsapp"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="img/PP2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Ngurah Bagus Arya Mahaputra</h5>
+                        <p class="card-text">Ganteng, giginya putih, kulitnya hitam, dan beliau adalah rival dwipa</p>
+                    </div>
+                    <div class="d-flex justify-content-evenly p-4">
+                        <i class="bi bi-facebook"></i>
+                        <i class="bi bi-linkedin"></i>
+                        <i class="bi bi-envelope-fill"></i>
+                        <i class="bi bi-whatsapp"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
