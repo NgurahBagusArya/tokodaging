63e18e47860608665d01353c7f5415ab5dce8e05
CancriPiscium <nyenkadendwika@gmail.com>
2023-11-17 08:37:12 +0800

    a

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,6 +1,7 @@
 </div> <!-- end of container class -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+<br><br><br><br><br><br><br><br><br>
 <footer>
     <div class="container text-center">
         <div class="footer row px-4 justify-content-center">
